ae99548206a03fc592843af4f195012b541b1426
cmis <cmis@cmis-hs01.(none)>
2015-09-03 09:56:54 +0700

    Fundamentals(completed)

---
+++ FundamentalsP9/FundamentalsP9.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+public class FundamentalsP9
+{
+        public static void main(String[] args)
+        {
+            String input1 = JOptionPane.showInputDialog("Enter your username!");
+            String input2 = JOptionPane.showInputDialog("Enter your password!(in numbers)");
+            int num = Integer.parseInt(input2);
+            if (input1.equals("Nam") && input2.equals(2301))
+            {
+                System.out.println("You have successfully logged in");
+            }
+            else
+            {
+                System.out.println("Login unsuccessful…try again");
+            }
+            
+        }
+}
