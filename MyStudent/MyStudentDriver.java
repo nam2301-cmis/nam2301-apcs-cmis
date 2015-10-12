038218ca3028c9ef53756b1a6f12d317f04b8646
cmis <cmis@cmis-hs01.(none)>
2015-10-12 11:54:30 +0700

    students

---
+++ MyStudent/MyStudentDriver.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class MyStudentDriver
+{
+    public static void main(String[] args)
+    {
+        MyStudent senior001 = new MyStudent();
+        System.out.println(senior001,JOptionPane.PLAIN_MESSAGE);
+        MyStudent junior002 = new
+        System.out.println(junior001);
+    }//end method main
+}//end class MyStudentDriver
