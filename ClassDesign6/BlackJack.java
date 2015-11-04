3e5df34eb3a77dbcc0a3b28db9d90824b61394c3
cmis <cmis@cmis-hs01.(none)>
2015-11-04 11:17:15 +0700

    classdesign6

--- ClassDesign6/BlackJack.java
+++ ClassDesign6/BlackJack.java
@@ -4,12 +4,12 @@ public class BlackJack
     public static void main(String args[])
     {
         //players and the dealer
-        String Dealer[]= new String[2];
-        String P1[]=new String[2];
-        String P2[]=new String[2];
-        String P3[]=new String[2];
-        String P4[]=new String[2];
-        String P5[]=new String[2];
+        Card Dealer[]= new Card[2];
+        Card P1[]=new Card[2];
+        Card P2[]=new Card[2];
+        Card P3[]=new Card[2];
+        Card P4[]=new Card[2];
+        Card P5[]=new Card[2];
         
         String rank[]={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
         int score[]= {1,2,3,4,5,6,7,8,9,10,10,10,10};
@@ -17,10 +17,9 @@ public class BlackJack
         //Dealer
         for(int i=0;i<Dealer.length;i++)
         {
-            int thecard=((int)(Math.random()*13));
-            int thesuit=((int)(Math.random()*4));
-            Dealer[i]= rank[i] + suit[i];
-            System.out.println(Dealer[i]);
+            
+            Dealer[i]= new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);;
+            System.out.println("Dealer "+Dealer[i]);
         }
         
         //Player1
@@ -28,8 +27,46 @@ public class BlackJack
         {
             int thecard=((int)(Math.random()*13));
             int thesuit=((int)(Math.random()*4));
-            P1[i]= rank[i] + suit[i];
-            System.out.println(P1[i]);
+            P1[i]=  new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);
+            System.out.println("Player1 "+P1[i]);
         }
+        
+        //Player2
+        for(int i=0;i<P2.length;i++)
+        {
+            int thecard=((int)(Math.random()*13));
+            int thesuit=((int)(Math.random()*4));
+            P2[i]=  new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);
+            System.out.println("Player2 "+P2[i]);
+        }
+        
+        //Player3
+        for(int i=0;i<P3.length;i++)
+        {
+            int thecard=((int)(Math.random()*13));
+            int thesuit=((int)(Math.random()*4));
+            P3[i]=  new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);
+            System.out.println("Player3 "+P3[i]);
+        }
+        
+        //Player4
+        for(int i=0;i<P4.length;i++)
+        {
+            int thecard=((int)(Math.random()*13));
+            int thesuit=((int)(Math.random()*4));
+            P4[i]=  new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);
+            System.out.println("Player4 "+P4[i]);
+        }
+        
+        //Player5
+        for(int i=0;i<P5.length;i++)
+        {
+            int thecard=((int)(Math.random()*13));
+            int thesuit=((int)(Math.random()*4));
+            P5[i]=  new Card(suit[(int)(Math.random()*3)],rank[(int)(Math.random()*13)]);
+            System.out.println("Player5 "+P5[i]);
+        }
+        
+        
     }//end method
 }
