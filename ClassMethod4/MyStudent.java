836dc1bec4f456d22acff583756d3846eef8d5dd
cmis <cmis@cmis-hs01.(none)>
2015-11-30 11:49:33 +0700

    classmethod4

--- ClassMethod4/MyStudent.java
+++ ClassMethod4/MyStudent.java
@@ -10,14 +10,13 @@ public class MyStudent
     private double GradeFineArts;
     private double GradeSocialScience;
     private String LetterGPA;
-    private double total=0.0;
-    
+
     //zero argument
     public MyStudent()
     {
         first = new String("John");
         last = new String("Doe");
-        LetterGPA=" ";
+ 
         ClassGrades =new double[5];
         ClassGrades[0]=4.0;
         ClassGrades[1]=4.0;
@@ -25,7 +24,7 @@ public class MyStudent
         ClassGrades[3]=4.0;
         ClassGrades[4]=4.0;
     }
-    
+
     //multi arguments
     public MyStudent(String first, String last,double GradeEnglish, double GradeMath, double GradeScience, double GradeFineArts, double GradeSocialScience,String LetterGPA)
     {
@@ -37,9 +36,9 @@ public class MyStudent
         this.ClassGrades[2] = GradeScience;
         this.ClassGrades[3] = GradeFineArts;
         this.ClassGrades[4] = GradeSocialScience;
-        this.LetterGPA=LetterGPA;
+  
     }
-    
+
     public double CalcGPA()
     {
         double total = 0.0;
@@ -49,12 +48,12 @@ public class MyStudent
             total +=ClassGrades[index];//short form
         }
         total/= ClassGrades.length;//short form
-        
+
         return total; 
-        
+
     }//end method CalcGPA
-    
-     public void setGPA(double English, double Math, double Science, double FineArts, double SocialScience)
+
+    public void setGPA(double English, double Math, double Science, double FineArts, double SocialScience)
     {
         this.ClassGrades[0]=English;
         this.ClassGrades[1]=Math;
@@ -62,28 +61,22 @@ public class MyStudent
         this.ClassGrades[3]=FineArts;
         this.ClassGrades[4]=SocialScience;
     }
-    
-    public double[] getClassGrades()
-    {
-        return ClassGrades;
-    }
+
     public String GPAletter()
     {
-     
-        
-        if(total>=4.0)
+        if(CalcGPA()>=4.0)
         {
-           LetterGPA="A";
+            LetterGPA="A";
         }
-        if(total>= 3.0 && total < 4.0)
+        else if(CalcGPA()>= 3.0 && CalcGPA() < 4.0)
         {
             LetterGPA="B";
         }
-        if(total>=2.0 && total < 3.0)
+        else if(CalcGPA()>=2.0 && CalcGPA() < 3.0)
         {
             LetterGPA="C";
         }
-        if(total>=1.0 && total < 2.0)
+        else if(CalcGPA()>=1.0 && CalcGPA()< 2.0)
         {
             LetterGPA="D";
         }
@@ -91,19 +84,19 @@ public class MyStudent
         {
             LetterGPA="F";
         }
-        
+
         return LetterGPA;
     }//end lettergrade
-    
+
     public String toString()
     {
         String output = new String();
-        output = "First name: " + first+"\n"+"Last name: "+last+"\n"+"Student's average GPA "+ CalcGPA()+"\n"
-                   +"John's Grade "+GPAletter()+"\n"+ClassGrades[0] + " " +ClassGrades[1]+" "+ClassGrades[2]+" "+ClassGrades[3]+" "+ClassGrades[4];
-        
-                   return output;
-        
+        output = "First name: " + first+"\n"+"Last name: "+last+"\n"+"Student's average GPA "+ CalcGPA()+"\n" 
+        +"John's Grade "+ LetterGPA +"\n"+ClassGrades[0] + " " +ClassGrades[1]+" "+ClassGrades[2]+" "+ClassGrades[3]+" "+ClassGrades[4];
+
+        return output;
+
     }
-    
+
 }
 
--- ClassMethod4/MyStudentDriver.java
+++ ClassMethod4/MyStudentDriver.java
@@ -3,13 +3,16 @@ public class MyStudentDriver
 {
     public static void main(String[] args)
     {
-        
+
         MyStudent student = new MyStudent();
+
+        student.CalcGPA();
+        student.GPAletter();
         System.out.println(student);
-        
-       
-        student.setGPA(4.0,4.0,4.0,4.0,4.0);
-       
+
+        student.setGPA(4.0,3.0,4.0,4.0,4.0);
+        student.CalcGPA();
+        student.GPAletter();
         System.out.println(student);
     }//end method main
 }//end class MyStudentDriver
