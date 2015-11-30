e2439b784769fbcd138bc2b0ef628ddf2bce2772
nam2301-cmis <nam2301@cmis.ac.th>
2015-11-30 21:35:41 +0700

    passwordP6

--- ClassMethod6/Password.java
+++ ClassMethod6/Password.java
@@ -3,9 +3,24 @@ public class Password
 {
     public static void main(String[] args)
     {
-        String password=new String(JOptionPane.showInputDialog("Enter a password[6 characters long, one non-alphanumeric character]"));
-        //if(String.equals())
+         int count =0;
+        String password=new String(JOptionPane.showInputDialog("Enter a password[at least 6 characters and at least one non-alphanumeric character (+, -, *, /, or @.)]"));
+        //variable that counts the weird letters.
+       
+      for(int i=0;i<password.length();i++)
+      {
+            if(password.substring(i).equals("+")||password.substring(i).equals("-")||password.substring(i).equals("*")||password.substring(i).equals("/")||password.substring(i).equals("@"))
         {
+            count+=1;
         }
+      }
+      if (password.length()<6 || count == 0)
+     {
+        System.out.println("Try again!");
+     }
+     else
+     {
+        System.out.println("Succesful!");
+     }
     }
 }
