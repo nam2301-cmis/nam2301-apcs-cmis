a19b297b65fb489837b4bbf41848457772ccf24d
nam2301-cmis <nam2301@cmis.ac.th>
2016-01-18 21:24:44 +0700

    classcomposition

--- ClassComposition1/MyPod.java
+++ ClassComposition1/MyPod.java
@@ -4,6 +4,7 @@ public class MyPod
     private String[] songlist;
     private int memory;
     
+    //zero-argument
     public MyPod()
     {
        this.color = new String("Blue");
@@ -11,15 +12,31 @@ public class MyPod
        this.memory = 32;
     }
     
+    //multi-argument
     public MyPod( String color, int memory)
        {
         this.color = new String( color ); 
+        this.memory = memory;
         this.songlist = new String[3];
-        this.songlist.add(new String("Holding On to You"));
-        //songlist[0] = new String("Holding On to You");
-        //songlist[1] = new String("Stressed Out");
-        //songlist[2] = new String("Magic");
+        //this.songlist.add(new String("Holding On to You"));
+        songlist[0] = new String("Holding On to You");
+        songlist[1] = new String("Stressed Out");
+        songlist[2] = new String("Magic");
            
        }
+      
+    public String toString()
+    {
+        String output = new String();
+        output = "My MyPod color is " + color + "\n" +
+                 "The memory capacity is " + memory + "\n";
+            for(String s : songlist)
+              {
+                 output += s + "\n";
+              }
+        return output;
+    }
+    
+    
  
-}
+} //end class 
---
+++ ClassComposition1/MyPodDriver.java
@@ -0,0 +1,9 @@
+public class MyPodDriver
+{
+    public static void main(String[] args)
+    {
+       
+        MyPod carPod = new MyPod("Mint",128);
+        System.out.println(carPod);
+    }
+}//end class
