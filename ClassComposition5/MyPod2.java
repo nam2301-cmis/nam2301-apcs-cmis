cec84c5f714b9d3722fe993190487dde566588b9
cmis <cmis@cmis-hs02.(none)>
2016-02-01 11:53:46 +0700

    classcompostionDONE

--- ClassComposition5/MyPod2.java
+++ ClassComposition5/MyPod2.java
@@ -1,23 +1,26 @@
-
+import java.util.ArrayList;
 public class MyPod2
 {
     private String color;
-    //private String[] songlist;
+    private ArrayList<String> songList = new ArrayList<String>();
     private int memory;
     
     //zero-argument
     public MyPod2()
     {
-       this.color = new String("Blue");
-       //this.songlist = new String[3];
-       this.memory = 32;
+           color = new String ("Mint");
+           memory = 32;
+           songList.add("Holding On to You");
+           songList.add("Stressed Out");
+           songList.add("Magic");
     }
     
     //multi-argument
-    public MyPod2( String color, int memory)
+    public MyPod2( String color, int memory,ArrayList<String> songList)
        {
         this.color = new String( color ); 
         this.memory = memory;
+        this.songList=songList;
         //this.songlist = new String[3];
         //this.songlist.add(new String("Holding On to You"));
         //songlist[0] = new String("Holding On to You");
@@ -32,7 +35,12 @@ public class MyPod2
     {
         String output = new String();
         output = "My MyPod color is " + color + "\n" +
-                 "The memory capacity is " + memory + "\n";
+                 "The memory capacity is " + memory + "\n"+
+                 "The song list:" + songList.size()+" ";
+                 for(String s: songList)
+                 {
+                    output += "\n"+s;
+                 }//for eachh loop
         return output;
     }
     
--- ClassComposition5/MyPod2Driver.java
+++ ClassComposition5/MyPod2Driver.java
@@ -4,29 +4,10 @@ public class MyPod2Driver
     public static void main(String[] args)
     {
        
-        MyPod2 carPod = new MyPod2("Mint",128);
+        MyPod2 carPod = new MyPod2();
         System.out.println(carPod);
         
-        //uses an ArrayList to store Songs instead of an array. Provide the same functionality as in Classcomposition4.
-       
-           ArrayList<String> songs = new ArrayList<String>();
-           songs.add("Holding On to You");
-           songs.add("Stressed Out");
-           songs.add("Magic");
-           System.out.println("Using an ArrayList to store Songs instead of an array.");
-           System.out.println("The length of the ArrayList is: "+ songs.size());
-           //Use a for loop to traverse the ArrayList and print the name of each person on a separate line.
-        for(int i=0;i<songs.size();i++)
-        {
-            System.out.println(songs.get(i));
-        }
         
-        //Use a separate for-each loop to traverse the ArrayList and print the name of each person on a separate line.
-        System.out.println("for-each loop....");
-        for (String s: songs)
-        {
-            System.out.println(s);
-        }
        
     }
 }//end class
