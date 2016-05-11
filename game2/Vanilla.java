e018f3cdbce63cde471d7a0624e2f85d8539ce05
cmis <cmis@cmis-hs06.(none)>
2016-05-11 11:56:06 +0700

    game

---
+++ game2/Vanilla.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Vanilla here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vanilla extends iceCream
+{
+    /**
+     * Act - do whatever the Vanilla wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ game2/cone2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class cone2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class cone2 extends cone
+{
+    /**
+     * Act - do whatever the cone2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
